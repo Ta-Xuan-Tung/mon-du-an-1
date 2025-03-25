cdbfc37970ac9fffdc9f20852871648b66078acc
Dsonnnnn <sondxph49374@gmail.com>
2025-03-25 08:42:35 +0700

    bai 4

--- views/admin/header.php
+++ views/admin/header.php
@@ -12,7 +12,7 @@
     <div class="container w-90">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?= ROOT_URL ?>">Ban hang</a>
+    <a class="navbar-brand" href="<?= ROOT_URL ?>">Trang bán hàng</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
